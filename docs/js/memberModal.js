30cc372840120248b98431497af0a2a36c4aa5e8
Ormair <milkyahonpo.ship7@gmail.com>
2020-04-25 13:50:36 +0900

    [FIx]

--- docs/js/memberModal.js
+++ docs/js/memberModal.js
@@ -1,6 +1,6 @@
 $('.member-box img').on('click', e => {
   let Target = $(e.target);
-  let imgPath = Target.attr("src");
+  let imgPath = Target.attr('src');
   let modal = $('#memberModal');
   modal.find('.modal-controller-download').attr('href', imgPath);
   modal.find('.modal-img').attr('src', imgPath);
