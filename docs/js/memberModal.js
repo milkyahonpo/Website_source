ef10119cdf9a41a295cd3234230e3343ddff7902
Ormair <milkyahonpo.ship7@gmail.com>
2020-04-25 13:47:37 +0900

    [Update]動的にモーダル生成を行うように変更しました

---
+++ docs/js/memberModal.js
@@ -0,0 +1,7 @@
+$('.member-box img').on('click', e => {
+  let Target = $(e.target);
+  let imgPath = Target.attr("src");
+  let modal = $('#memberModal');
+  modal.find('.modal-controller-download').attr('href', imgPath);
+  modal.find('.modal-img').attr('src', imgPath);
+})
